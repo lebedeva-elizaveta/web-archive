7f8e6fdbde7505167d8c152bba37db2931c97cf0
Elizaveta Lebedeva <elizaveta.lebedeva.03@inbox.ru>
2025-04-29 00:09:22 +0300

    return main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from app.config import settings
-from app.models import db
-from app.services.selenium_service import get_driver
+from .config import settings
+from .models import db
 
 app = Flask(__name__)
 app.config.from_object(settings)
@@ -12,13 +11,5 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 with app.app_context():
-    from app import routes
+    from . import routes
     db.create_all()
-
-
-@app.teardown_appcontext
-def shutdown_driver(exception=None):
-    """ Закрытие драйвера после завершения работы приложения """
-    if driver:
-        driver.quit()
-        print('end')
